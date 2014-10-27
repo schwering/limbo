3c7da0028c136dad4002e4ac85a82c77e75cce2e
Christoph Schwering <schwering@kbsg.rwth-aachen.de>
2014-10-27 04:07:07 +0100

    Added unit test for clause subsumption.

---
+++ tests/clause_test.cc
@@ -0,0 +1,60 @@
+// vim:filetype=cpp:textwidth=80:shiftwidth=2:softtabstop=2:expandtab
+// Copyright 2014 schwering@kbsg.rwth-aachen.de
+
+#include <gtest/gtest.h>
+#include <./clause.h>
+
+using namespace esbl;
+
+static Term::Factory f;
+static StdName n0 = f.CreateStdName(0, 1);
+static StdName n1 = f.CreateStdName(1, 1);
+static StdName n2 = f.CreateStdName(2, 1);
+static StdName n3 = f.CreateStdName(3, 1);
+static StdName n4 = f.CreateStdName(4, 1);
+static StdName n5 = f.CreateStdName(5, 1);
+static StdName n6 = f.CreateStdName(6, 1);
+static Variable x0 = f.CreateVariable(1);
+static Variable x1 = f.CreateVariable(1);
+static Variable x2 = f.CreateVariable(1);
+static Variable x3 = f.CreateVariable(1);
+static Variable x4 = f.CreateVariable(1);
+static Variable x5 = f.CreateVariable(1);
+static Variable x6 = f.CreateVariable(1);
+
+static std::set<StdName> names{n0,n1,n2,n3,n4,n5,n6};
+static StdName::SortedSet hplus{{1, names}};
+
+static Atom::PredId P = 1;
+static Atom::PredId Q = 2;
+
+TEST(clause, subsumption)
+{
+  Clause empty(true, Ewff::TRUE, GroundClause());
+  Clause c1(true,
+            Ewff({ Ewff::Conj({{x1,n1}},
+                              {{x2,x3}},
+                              {{x2,n2}, {x3,n3}},
+                              {{x1,x2}})}),
+            GroundClause({ Literal({x3}, true, P, {x1,x2}),
+                           Literal({x3}, false, P, {x1,x2}) }));
+  Clause c2(false,
+            Ewff({ Ewff::Conj({},
+                              {{x4,x5}},
+                              {},
+                              {{x5,x6}})}),
+            GroundClause({ Literal({x4}, true, P, {x5,x6}),
+                           Literal({x6}, false, Q, {x4,x5}) }));
+  GroundClause d1({Literal({n2,n4}, true, P, {n1,n4}),
+                   Literal({n2,n4}, false, P, {n1,n4})});
+  GroundClause d2({Literal({n4}, true, P, {n4,n6}),
+                   Literal({n6}, false, Q, {n4,n4})});
+
+  EXPECT_TRUE(empty.Subsumes(d1));
+  EXPECT_TRUE(empty.Subsumes(d2));
+  EXPECT_TRUE(c1.Subsumes(d1));
+  EXPECT_TRUE(c2.Subsumes(d2));
+  EXPECT_FALSE(c1.Subsumes(d2));
+  EXPECT_FALSE(c2.Subsumes(d1));
+}
+
