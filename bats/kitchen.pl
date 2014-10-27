45786a5a22afe55f45c8619e6a3e2b677699afb5
Christoph Schwering <schwering@kbsg.rwth-aachen.de>
2014-10-27 19:03:30 +0100

    Changed kitchen BAT for demo.
    Performance is very bad.
    The resulting setups are huge and grow linearly in the number of
    actions, and query evaluation seems to grow quadratically in the size
    of the setup.
    
    The split optimizations in setup are still worth their cost (replacing
    setup_clause_small_pel() with setup_full_pel() costs about 20% and
    removing the expensive part of setup_relevant_split() costs 1000%).

--- bats/kitchen.pl
+++ bats/kitchen.pl
@@ -4,121 +4,43 @@
 % and cannot use higher-ary functions. Hence, we need to encode pickup(X) as
 % pickup_X and cannot use Prolog unification.
 
-sort_name(action, pickup_o1).
-sort_name(action, pickup_o2).
-sort_name(action, pickup_o3).
-sort_name(action, pickup_o4).
-sort_name(action, pickup_o5).
-sort_name(action, drop_o1).
-sort_name(action, drop_o2).
-sort_name(action, drop_o3).
-sort_name(action, drop_o4).
-sort_name(action, drop_o5).
-sort_name(action, sense_present_o1).
-sort_name(action, sense_present_o2).
-sort_name(action, sense_present_o3).
-sort_name(action, sense_present_o4).
-sort_name(action, sense_present_o5).
-sort_name(action, sense_box1_o1).
-sort_name(action, sense_box1_o2).
-sort_name(action, sense_box1_o3).
-sort_name(action, sense_box1_o4).
-sort_name(action, sense_box1_o5).
-sort_name(action, sense_box2_o1).
-sort_name(action, sense_box2_o2).
-sort_name(action, sense_box2_o3).
-sort_name(action, sense_box2_o4).
-sort_name(action, sense_box2_o5).
+sense_is_object(O, A) :- sort_name(object, O), atom_concat(O, '_is_object', A).
+sense_is_box(O, A) :- sort_name(object, O), atom_concat(O, '_is_box', A).
+sense_has_type(O, T, A) :- sort_name(object, O), sort_name(type, T), atom_concat(O, '_has_type_', A1), atom_concat(A1, T, A).
 
+sort_name(object, o0).
 sort_name(object, o1).
-sort_name(object, o2).
-sort_name(object, o3).
-sort_name(object, o4).
-sort_name(object, o5).
+%sort_name(object, o2).
+%sort_name(object, o3).
+%sort_name(object, o4).
+%sort_name(object, o5).
+%sort_name(object, o6).
+%sort_name(object, o7).
 
-sort_name(type, box1).
-sort_name(type, box2).
+sort_name(box, boxA).
+sort_name(box, boxB).
+sort_name(box, boxC).
 
-%box(
-%    'POSS'(A) <-> A = sense_table ^ robot_at_table v
-%                  A = sense_counter ^ pos(counter) v
-%                  A = sense_o1 v
-%                  A = sense_o2 v
-%                  A = sense_o3 v
-%                  A = drop_o1 ^ (robot_at_table <-> object_at_table(o1)) v
-%                  A = drop_o2 ^ (robot_at_table <-> object_at_table(o2)) v
-%                  A = drop_o3 ^ (robot_at_table <-> object_at_table(o3)) v
-%                  A = pickup_o1 ^ (robot_at_table <-> object_at_table(o1)) v
-%                  A = pickup_o2 ^ (robot_at_table <-> object_at_table(o2)) v
-%                  A = pickup_o3 ^ (robot_at_table <-> object_at_table(o3))
-%) :- put_sort(A, action).
+sort_name(type, B) :- sort_name(box, B).
 
-% Saves several thousand clauses:
-box(A = sense_present_o1 v A = sense_present_o2 v A = sense_present_o3 v A = sense_present_o4 v A = sense_present_o5 -> ('POSS'(A) <-> true)) :- put_sort(A, action).
-box(A = sense_box1_o1 v A = sense_box2_o1 v A = pickup_o1 v A = drop_o1 -> ('POSS'(A) <-> (robot_at_table -> object_at_table(o1)))) :- put_sort(A, action).
-box(A = sense_box1_o2 v A = sense_box2_o2 v A = pickup_o2 v A = drop_o2 -> ('POSS'(A) <-> (robot_at_table -> object_at_table(o2)))) :- put_sort(A, action).
-box(A = sense_box1_o3 v A = sense_box2_o3 v A = pickup_o3 v A = drop_o3 -> ('POSS'(A) <-> (robot_at_table -> object_at_table(o3)))) :- put_sort(A, action).
-box(A = sense_box1_o4 v A = sense_box2_o4 v A = pickup_o4 v A = drop_o4 -> ('POSS'(A) <-> (robot_at_table -> object_at_table(o4)))) :- put_sort(A, action).
-box(A = sense_box1_o5 v A = sense_box2_o5 v A = pickup_o5 v A = drop_o5 -> ('POSS'(A) <-> (robot_at_table -> object_at_table(o5)))) :- put_sort(A, action).
+sort_name(action, A) :- sense_is_object(_, A).
+sort_name(action, A) :- sense_is_box(_, A).
+sort_name(action, A) :- sense_has_type(_, _, A).
 
-% Saves several thousand clauses:
-box(A = sense_present_o1 -> ('SF'(A) <-> (robot_at_table -> object_at_table(o1)))) :- put_sort(A, action).
-box(A = sense_present_o2 -> ('SF'(A) <-> (robot_at_table -> object_at_table(o2)))) :- put_sort(A, action).
-box(A = sense_present_o3 -> ('SF'(A) <-> (robot_at_table -> object_at_table(o3)))) :- put_sort(A, action).
-box(A = sense_present_o4 -> ('SF'(A) <-> (robot_at_table -> object_at_table(o4)))) :- put_sort(A, action).
-box(A = sense_present_o5 -> ('SF'(A) <-> (robot_at_table -> object_at_table(o5)))) :- put_sort(A, action).
-box(A = sense_box1_o1    -> ('SF'(A) <-> type(o1,box1))) :- put_sort(A, action).
-box(A = sense_box1_o2    -> ('SF'(A) <-> type(o2,box1))) :- put_sort(A, action).
-box(A = sense_box1_o3    -> ('SF'(A) <-> type(o3,box1))) :- put_sort(A, action).
-box(A = sense_box1_o4    -> ('SF'(A) <-> type(o4,box1))) :- put_sort(A, action).
-box(A = sense_box1_o5    -> ('SF'(A) <-> type(o5,box1))) :- put_sort(A, action).
-box(A = sense_box2_o1    -> ('SF'(A) <-> type(o1,box2))) :- put_sort(A, action).
-box(A = sense_box2_o2    -> ('SF'(A) <-> type(o2,box2))) :- put_sort(A, action).
-box(A = sense_box2_o3    -> ('SF'(A) <-> type(o3,box2))) :- put_sort(A, action).
-box(A = sense_box2_o4    -> ('SF'(A) <-> type(o4,box2))) :- put_sort(A, action).
-box(A = sense_box2_o5    -> ('SF'(A) <-> type(o5,box2))) :- put_sort(A, action).
+is_one_of(_, [], ~true).
+is_one_of(V, [T|Ts], V = T v Phi) :- is_one_of(V, Ts, Phi).
 
+type_disj(_, [], ~true).
+type_disj(O, [T|Ts], type(O,T) v Phi) :- type_disj(O, Ts, Phi).
+
+box('SF'(A) <-> object(O)) :- sense_is_object(O, A).
+box('SF'(A) <-> TypeDisj) :- sense_is_box(O, A), findall(Box, sort_name(box, Box), Boxes), type_disj(O, Boxes, TypeDisj).
+box('SF'(A) <-> type(O, T)) :- sense_has_type(O, T, A).
+
+box(A:object(O) <-> object(O)) :- put_sort(A, action), put_sort(O, object).
 box(A:type(O,T) <-> type(O,T)) :- put_sort(A, action), put_sort(O, object), put_sort(T, type).
 
-box(A:robot_at_table <-> A = goto_table v
-                         A \= goto_table ^ robot_at_table) :- put_sort(A, action).
-
-box(A:object_at_table(O) <-> (A = drop_o1 ^ O = o1 v
-                              A = drop_o2 ^ O = o2 v
-                              A = drop_o3 ^ O = o3 v
-                              A = drop_o4 ^ O = o4 v
-                              A = drop_o5 ^ O = o5) ^ robot_at_table v
-                             (A \= pickup_o1 ^ O = o1 v
-                              A \= pickup_o2 ^ O = o2 v
-                              A \= pickup_o3 ^ O = o3 v
-                              A \= pickup_o4 ^ O = o4 v
-                              A \= pickup_o5 ^ O = o5) ^ object_at_table(O)
-) :- put_sort(A, action),
-     put_sort(O, object).
-
-box(A:holding(O) <-> A = pickup_o1 ^ O = o1 v
-                     A = pickup_o2 ^ O = o2 v
-                     A = pickup_o3 ^ O = o3 v
-                     A = pickup_o4 ^ O = o4 v
-                     A = pickup_o5 ^ O = o5 v
-                     A \= drop_o1 ^ O = o1 ^ holding(O) v
-                     A \= drop_o2 ^ O = o2 ^ holding(O) v
-                     A \= drop_o3 ^ O = o3 ^ holding(O) v
-                     A \= drop_o4 ^ O = o4 ^ holding(O) v
-                     A \= drop_o5 ^ O = o5 ^ holding(O)
-) :- put_sort(A, action),
-     put_sort(O, object).
-
-% Costs about 300 clauses:
-%box(A = drop_o1 -> (A:object_at_table(O) <-> O = o1 ^ robot_at_table v object_at_table(O))) :- put_sort(A, action), put_sort(O, object), put_sort(L, location).
-%box(A = drop_o2 -> (A:object_at_table(O) <-> O = o2 ^ robot_at_table v object_at_table(O))) :- put_sort(A, action), put_sort(O, object), put_sort(L, location).
-%box(A = drop_o3 -> (A:object_at_table(O) <-> O = o3 ^ robot_at_table v object_at_table(O))) :- put_sort(A, action), put_sort(O, object), put_sort(L, location).
-%box(A = pickup_o1 -> (A:object_at_table(O) <-> O \= o1 ^ object_at_table(O))) :- put_sort(A, action), put_sort(O, object), put_sort(L, location).
-%box(A = pickup_o2 -> (A:object_at_table(O) <-> O \= o2 ^ object_at_table(O))) :- put_sort(A, action), put_sort(O, object), put_sort(L, location).
-%box(A = pickup_o3 -> (A:object_at_table(O) <-> O \= o3 ^ object_at_table(O))) :- put_sort(A, action), put_sort(O, object), put_sort(L, location).
-%box(A \= drop_o1 ^ A \= drop_o2 ^ A \= drop_o3 ^ A \= pickup_o1 ^ A \= pickup_o2 ^ A \= pickup_o3 -> (A:object_at_table(O) <-> object_at_table(O))) :- put_sort(A, action), put_sort(O, object), put_sort(L, location).
-
-static(robot_at_table).
+static(_) :- fail.
 
 belief(_ => _) :- fail.
 
