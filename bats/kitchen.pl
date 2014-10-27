88838c169f6dfb626bb6dc82c3bfacb8b35ec2fe
Christoph Schwering <schwering@kbsg.rwth-aachen.de>
2014-10-27 19:03:30 +0100

    Preliminary version of the demo kitchen BAT.
    The performance in the C implementation was very bad. The reason was:
    
    <QUOTE about C-implementation>
    
    The resulting setups are huge and grow linearly in the number of
    actions, and query evaluation seems to grow quadratically in the size
    of the setup.
    
    The split optimizations in setup are still worth their cost (replacing
    setup_clause_small_pel() with setup_full_pel() costs about 20% and
    removing the expensive part of setup_relevant_split() costs 1000%).
    
    </QUOTE about C-implementation>

--- bats/kitchen.pl
+++ bats/kitchen.pl
@@ -4,106 +4,43 @@
 % and cannot use higher-ary functions. Hence, we need to encode pickup(X) as
 % pickup_X and cannot use Prolog unification.
 
-sort_name(action, pickup_o1).
-sort_name(action, pickup_o2).
-sort_name(action, pickup_o3).
-sort_name(action, drop_o1).
-sort_name(action, drop_o2).
-sort_name(action, drop_o3).
-sort_name(action, sense_present_o1).
-sort_name(action, sense_present_o2).
-sort_name(action, sense_present_o3).
-sort_name(action, sense_cup_o1).
-sort_name(action, sense_cup_o2).
-sort_name(action, sense_cup_o3).
-sort_name(action, sense_mug_o1).
-sort_name(action, sense_mug_o2).
-sort_name(action, sense_mug_o3).
+sense_is_object(O, A) :- sort_name(object, O), atom_concat(O, '_is_object', A).
+sense_is_box(O, A) :- sort_name(object, O), atom_concat(O, '_is_box', A).
+sense_has_type(O, T, A) :- sort_name(object, O), sort_name(type, T), atom_concat(O, '_has_type_', A1), atom_concat(A1, T, A).
 
+sort_name(object, o0).
 sort_name(object, o1).
-sort_name(object, o2).
-sort_name(object, o3).
+%sort_name(object, o2).
+%sort_name(object, o3).
+%sort_name(object, o4).
+%sort_name(object, o5).
+%sort_name(object, o6).
+%sort_name(object, o7).
 
-sort_name(location, counter).
-sort_name(location, table).
+sort_name(box, boxA).
+sort_name(box, boxB).
+sort_name(box, boxC).
 
-sort_name(type, cup).
-sort_name(type, mug).
+sort_name(type, B) :- sort_name(box, B).
 
-%box(
-%    'POSS'(A) <-> A = sense_table ^ pos(table) v
-%                  A = sense_counter ^ pos(counter) v
-%                  A = sense_o1 v
-%                  A = sense_o2 v
-%                  A = sense_o3 v
-%                  A = drop_o1 ^ (pos(L) <-> at(o1,L)) v
-%                  A = drop_o2 ^ (pos(L) <-> at(o2,L)) v
-%                  A = drop_o3 ^ (pos(L) <-> at(o3,L)) v
-%                  A = pickup_o1 ^ (pos(L) <-> at(o1,L)) v
-%                  A = pickup_o2 ^ (pos(L) <-> at(o2,L)) v
-%                  A = pickup_o3 ^ (pos(L) <-> at(o3,L))
-%) :- put_sort(A, action),
-%     put_sort(L, location).
+sort_name(action, A) :- sense_is_object(_, A).
+sort_name(action, A) :- sense_is_box(_, A).
+sort_name(action, A) :- sense_has_type(_, _, A).
 
-% Saves several thousand clauses:
-box(A = sense_present_o1                                              -> ('POSS'(A) <-> true)) :- put_sort(A, action).
-box(A = sense_present_o2                                              -> ('POSS'(A) <-> true)) :- put_sort(A, action).
-box(A = sense_present_o3                                              -> ('POSS'(A) <-> true)) :- put_sort(A, action).
-box(A = sense_cup_o1 v A = sense_mug_o1 v A = pickup_o1 v A = drop_o1 -> ('POSS'(A) <-> (pos(L) -> at(o1,L)))) :- put_sort(A, action), put_sort(L, location).
-box(A = sense_cup_o2 v A = sense_mug_o2 v A = pickup_o2 v A = drop_o2 -> ('POSS'(A) <-> (pos(L) -> at(o2,L)))) :- put_sort(A, action), put_sort(L, location).
-box(A = sense_cup_o3 v A = sense_mug_o3 v A = pickup_o3 v A = drop_o3 -> ('POSS'(A) <-> (pos(L) -> at(o3,L)))) :- put_sort(A, action), put_sort(L, location).
+is_one_of(_, [], ~true).
+is_one_of(V, [T|Ts], V = T v Phi) :- is_one_of(V, Ts, Phi).
 
-% Saves several thousand clauses:
-box(A = sense_present_o1 -> ('SF'(A) <-> (pos(L) -> at(o1,L)))) :- put_sort(A, action), put_sort(L, location).
-box(A = sense_present_o2 -> ('SF'(A) <-> (pos(L) -> at(o2,L)))) :- put_sort(A, action), put_sort(L, location).
-box(A = sense_present_o3 -> ('SF'(A) <-> (pos(L) -> at(o3,L)))) :- put_sort(A, action), put_sort(L, location).
-box(A = sense_cup_o1     -> ('SF'(A) <-> type(o1,cup))) :- put_sort(A, action).
-box(A = sense_cup_o2     -> ('SF'(A) <-> type(o2,cup))) :- put_sort(A, action).
-box(A = sense_cup_o3     -> ('SF'(A) <-> type(o3,cup))) :- put_sort(A, action).
-box(A = sense_mug_o1     -> ('SF'(A) <-> type(o1,mug))) :- put_sort(A, action).
-box(A = sense_mug_o2     -> ('SF'(A) <-> type(o2,mug))) :- put_sort(A, action).
-box(A = sense_mug_o3     -> ('SF'(A) <-> type(o3,mug))) :- put_sort(A, action).
+type_disj(_, [], ~true).
+type_disj(O, [T|Ts], type(O,T) v Phi) :- type_disj(O, Ts, Phi).
 
+box('SF'(A) <-> object(O)) :- sense_is_object(O, A).
+box('SF'(A) <-> TypeDisj) :- sense_is_box(O, A), findall(Box, sort_name(box, Box), Boxes), type_disj(O, Boxes, TypeDisj).
+box('SF'(A) <-> type(O, T)) :- sense_has_type(O, T, A).
+
+box(A:object(O) <-> object(O)) :- put_sort(A, action), put_sort(O, object).
 box(A:type(O,T) <-> type(O,T)) :- put_sort(A, action), put_sort(O, object), put_sort(T, type).
 
-box(A:loc(L) <-> A = goto_table ^ L = table v
-                 A = goto_counter ^ L = counter v
-                 A \= goto_table ^ A \= goto_counter ^ pos(L)) :- put_sort(A, action), put_sort(L, location).
-
-% Doesn't save any clauses:
-%box(A = goto_table -> (A:loc(L) <-> L = table)) :- put_sort(A, action), put_sort(L, location).
-%box(A = goto_counter -> (A:loc(L) <-> L = counter)) :- put_sort(A, action), put_sort(L, location).
-%box(A \= goto_table ^ A \= goto_counter -> (A:loc(L) <-> pos(L))) :- put_sort(A, action), put_sort(L, location).
-
-box(A:at(O,L) <-> (A = drop_o1 ^ O = o1 v
-                   A = drop_o2 ^ O = o2 v
-                   A = drop_o3 ^ O = o3) ^ pos(L) v
-                  (A \= pickup_o1 ^ O = o1 v
-                   A \= pickup_o2 ^ O = o2 v
-                   A \= pickup_o3 ^ O = o3) ^ at(O,L)
-) :- put_sort(A, action),
-     put_sort(O, object),
-     put_sort(L, location).
-
-box(A:holding(O) <-> A = pickup_o1 ^ O = o1 v
-                     A = pickup_o2 ^ O = o2 v
-                     A = pickup_o3 ^ O = o3 v
-                     A \= drop_o1 ^ O = o1 ^ holding(O) v
-                     A \= drop_o2 ^ O = o2 ^ holding(O) v
-                     A \= drop_o3 ^ O = o3 ^ holding(O)
-) :- put_sort(A, action),
-     put_sort(O, object).
-
-% Costs about 300 clauses:
-%box(A = drop_o1 -> (A:at(O,L) <-> O = o1 ^ pos(L) v at(O,L))) :- put_sort(A, action), put_sort(O, object), put_sort(L, location).
-%box(A = drop_o2 -> (A:at(O,L) <-> O = o2 ^ pos(L) v at(O,L))) :- put_sort(A, action), put_sort(O, object), put_sort(L, location).
-%box(A = drop_o3 -> (A:at(O,L) <-> O = o3 ^ pos(L) v at(O,L))) :- put_sort(A, action), put_sort(O, object), put_sort(L, location).
-%box(A = pickup_o1 -> (A:at(O,L) <-> O \= o1 ^ at(O,L))) :- put_sort(A, action), put_sort(O, object), put_sort(L, location).
-%box(A = pickup_o2 -> (A:at(O,L) <-> O \= o2 ^ at(O,L))) :- put_sort(A, action), put_sort(O, object), put_sort(L, location).
-%box(A = pickup_o3 -> (A:at(O,L) <-> O \= o3 ^ at(O,L))) :- put_sort(A, action), put_sort(O, object), put_sort(L, location).
-%box(A \= drop_o1 ^ A \= drop_o2 ^ A \= drop_o3 ^ A \= pickup_o1 ^ A \= pickup_o2 ^ A \= pickup_o3 -> (A:at(O,L) <-> at(O,L))) :- put_sort(A, action), put_sort(O, object), put_sort(L, location).
-
-static(pos(table)).
+static(_) :- fail.
 
 belief(_ => _) :- fail.
 
--- examples/kitchen.pl
+++ examples/kitchen.pl
@@ -1,7 +1,7 @@
 % vim:filetype=prolog:textwidth=80:shiftwidth=4:softtabstop=4:expandtab
 %
 % Run this file with the following command:
-%   $ eclipse-clp -f examples/kr2014.pl
+%   $ eclipse-clp -f examples/kitchen.pl
 
 % We just need that for the operator declarations:
 :- ['../bats/proper-plus.pl'].
@@ -20,7 +20,7 @@
 :- external(context_entails/3, p_context_entails).
 
 % Create a context and store it in an extra-logical variable:
-:- kcontext(Ctx), context_guarantee_consistency(Ctx, 1), context_store(myctx, Ctx).
+%:- kcontext(Ctx), context_guarantee_consistency(Ctx, 1), context_store(myctx, Ctx).
 
 % Now test the properties (some are taken from the KR-2014 paper, some are
 % additional tests; they also match the kr2014.c example):
@@ -32,15 +32,57 @@ measure(Ctx, Call) :-
     T is T1 - T0,
     write('OK: '), writeln(T).
 
-:- measure(Ctx, context_entails(Ctx, 1, p v ~p)).
-:- measure(Ctx, \+ context_entails(Ctx, 1, p v q)).
-:- measure(Ctx, context_entails(Ctx, 1, p v q v ~(p ^ q))).
-:- measure(Ctx, context_entails(Ctx, 1, p v ~(p ^ q) v q)).
-:- measure(Ctx, context_entails(Ctx, 1, p(n) v ~p(n))).
-:- measure(Ctx, context_entails(Ctx, 1, p(1) v ~p(1))).
-:- measure(Ctx, context_entails(Ctx, 1, forall(X, p(X) v ~p(X)))).
-:- writeln(ok).
-%:- measure(Ctx, context_entails(Ctx, 1, (p(1) v p(1)) -> exists(X, p(X)))).
+%:- measure(Ctx, context_entails(Ctx, 1, p v ~p)).
+%:- measure(Ctx, \+ context_entails(Ctx, 1, p v q)).
+%:- measure(Ctx, context_entails(Ctx, 1, p v q v ~(p ^ q))).
+%:- measure(Ctx, context_entails(Ctx, 1, p v ~(p ^ q) v q)).
+%:- measure(Ctx, context_entails(Ctx, 1, p(n) v ~p(n))). % triggers new grounding
+%:- measure(Ctx, context_entails(Ctx, 1, p(1) v ~p(1))).
+%:- measure(Ctx, context_entails(Ctx, 1, forall(X, p(X) v ~p(X)))).
+%:- writeln(ok).
+
+
+:-  profile((
+    kcontext(Ctx), context_guarantee_consistency(Ctx, 1), context_store(myctx, Ctx),
+    context_entails(Ctx, 1, type(o1, boxA) v ~type(o1, boxA)),
+    \+ context_entails(Ctx, 1, type(o0, boxA)),
+    \+ context_entails(Ctx, 1, exists(X, type(o0, X))),
+    \+ context_entails(Ctx, 1, exists(X, ~type(o0, X))),
+    \+ context_entails(Ctx, 1, forall(X, type(o1, X))),
+    \+ context_entails(Ctx, 1, forall(X, ~type(o1, X))),
+    \+ context_entails(Ctx, 1, type(o1, boxA) v type(o1, boxB) v type(o1, boxC)),
+    \+ context_entails(Ctx, 1, type(o1, boxA)),
+    \+ context_entails(Ctx, 1, type(o1, boxB)),
+    \+ context_entails(Ctx, 1, type(o1, boxC)),
+    true)),
+
+    profile((
+    context_exec(Ctx, o0_has_type_boxA, true),
+    context_entails(Ctx, 1, type(o0, boxA)),
+    context_entails(Ctx, 1, exists(X, type(o0, X))),
+    \+ context_entails(Ctx, 1, exists(X, ~type(o0, X))),
+    \+ context_entails(Ctx, 1, forall(X, type(o1, X))),
+    \+ context_entails(Ctx, 1, forall(X, ~type(o1, X))),
+    \+ context_entails(Ctx, 1, type(o1, boxA) v type(o1, boxB) v type(o1, boxC)),
+    \+ context_entails(Ctx, 1, type(o1, boxA)),
+    \+ context_entails(Ctx, 1, type(o1, boxB)),
+    \+ context_entails(Ctx, 1, type(o1, boxC)),
+    true)),
+
+    profile((
+    context_exec(Ctx, o1_is_box, true),
+    context_entails(Ctx, 1, type(o0, boxA)),
+    context_entails(Ctx, 1, exists(X, type(o0, X))),
+    \+ context_entails(Ctx, 1, exists(X, ~type(o0, X))),
+    \+ context_entails(Ctx, 1, forall(X, type(o1, X))),
+    \+ context_entails(Ctx, 1, forall(X, ~type(o1, X))),
+    context_entails(Ctx, 1, type(o1, boxA) v type(o1, boxB) v type(o1, boxC)),
+    \+ context_entails(Ctx, 1, type(o1, boxA)),
+    \+ context_entails(Ctx, 1, type(o1, boxB)),
+    \+ context_entails(Ctx, 1, type(o1, boxC)),
+    true)),
+
+    writeln('very good').
 
 :- exit(0).
 
