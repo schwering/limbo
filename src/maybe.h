45b3b128cc44337fec935770d0698db5c0e55d92
Christoph Schwering <schwering@kbsg.rwth-aachen.de>
2014-11-12 01:46:19 +0100

    Maybe can handle objects like std::unique_ptr.

--- src/maybe.h
+++ src/maybe.h
@@ -5,6 +5,7 @@
 #define SRC_MAYBE_H_
 
 #include <cassert>
+#include <memory>
 #include <utility>
 
 namespace esbl {
@@ -16,9 +17,9 @@ struct Maybe {
 template<typename T>
 struct Maybe<T> {
   Maybe()                                           : succ(false) {}
-  explicit Maybe(T&& val)                           : succ(true), val(val) {}  // NOLINT
+  explicit Maybe(T&& val)                           : succ(true), val(std::forward<T>(val)) {}  // NOLINT
   template<typename U> explicit Maybe(const U& val) : succ(true), val(val) {}
-  Maybe(bool succ, T&& val)                         : succ(succ), val(val) {}  // NOLINT
+  Maybe(bool succ, T&& val)                         : succ(succ), val(std::forward<T>(val)) {}  // NOLINT
 
   Maybe(const Maybe&) = default;
   Maybe(Maybe&&) = default;
@@ -38,7 +39,7 @@ struct Maybe<T> {
 
 template<typename T1, typename T2>
 struct Maybe<T1, T2> {
-  Maybe()                                                                           : succ(false) {}
+  Maybe()                                                                           : succ(false) {}  // NOLINT
   explicit Maybe(T1&& val1, T2&& val2)                                              : succ(true), val1(val1), val2(val2) {}  // NOLINT
   template<typename U1, typename U2> explicit Maybe(const U1& val1, const U2& val2) : succ(true), val1(val1), val2(val2) {}  // NOLINT
   Maybe(bool succ, T1&& val1, T2&& val2)                                            : succ(succ), val1(val1), val2(val2) {}  // NOLINT
@@ -71,12 +72,12 @@ constexpr NothingType Nothing = NothingType();
 
 template<typename... Types>
 Maybe<Types...> Just(Types&&... val) {  // NOLINT
-  return Maybe<Types...>(val...);
+  return Maybe<Types...>(std::forward<Types>(val)...);  // NOLINT
 }
 
 template<typename... Types>
 Maybe<Types...> Perhaps(bool succ, Types&&... val) {  // NOLINT
-  return Maybe<Types...>(succ, val...);
+  return Maybe<Types...>(succ, std::forward<Types>(val)...);  // NOLINT
 }
 
 }  // namespace esbl
