ba6bd44d55b8393dddb9a82d7173453e3b5fdddd
Christoph Schwering <schwering@kbsg.rwth-aachen.de>
2016-11-12 11:48:08 +1100

    Added IntMap unit test.

---
+++ tests/intmap.cc
@@ -0,0 +1,48 @@
+// vim:filetype=cpp:textwidth=80:shiftwidth=2:softtabstop=2:expandtab
+// Copyright 2014--2016 Christoph Schwering
+
+#include <gtest/gtest.h>
+#include "./intmap.h"
+
+namespace lela {
+
+template<typename T>
+size_t length(T r) { return std::distance(r.begin(), r.end()); }
+
+TEST(IntMap, general) {
+  IntMap<int, std::string> map;
+  map[0] = "zero";
+  map[2] = "two";
+  EXPECT_EQ(map[0], "zero");
+  EXPECT_EQ(map[1], "");
+  EXPECT_EQ(map[2], "two");
+  EXPECT_EQ(length(map.keys()), 3);
+  EXPECT_EQ(map.size(), 3);
+
+  const IntMap<int, std::string> map2 = map;
+  EXPECT_EQ(map2[0], "zero");
+  EXPECT_EQ(map2[1], "");
+  EXPECT_EQ(map2[2], "two");
+
+  map[1] = "one";
+  EXPECT_EQ(map[0], "zero");
+  EXPECT_EQ(map[1], "one");
+  EXPECT_EQ(map[2], "two");
+
+  EXPECT_EQ(map2[0], "zero");
+  EXPECT_EQ(map2[1], "");
+  EXPECT_EQ(map2[2], "two");
+
+  map.set_null_value("null");
+  map[4] = "four";
+  EXPECT_EQ(length(map.keys()), 5);
+  EXPECT_EQ(map.size(), 5);
+  EXPECT_EQ(map[0], "zero");
+  EXPECT_EQ(map[1], "one");
+  EXPECT_EQ(map[2], "two");
+  EXPECT_EQ(map[3], "null");
+  EXPECT_EQ(map[4], "four");
+}
+
+}  // namespace lela
+
